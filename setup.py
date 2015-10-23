5c00a2de8b797deffdc045fc932ed59c05d3b8d9
Jake <me@jh.gg>
2015-10-23 10:25:07 -0400

    Bump version to v0.7.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ ext_modules = [
 
 setup(
     name='pybloof',
-    version='0.6',
+    version='0.7',
     author='Jake Heinz',
     author_email='me@jh.gg',
     url="http://github.com/jhgg/pybloof",
